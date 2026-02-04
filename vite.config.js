4d5d7bd20fefcf88bb8079f1e4ffd2d2ca6be308
woopsa-sid <sid@cdtsupport.in>
2026-02-05 02:23:24 +0530

    chnage

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+//  https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': "http://72.62.227.2:4000/"  // Backend runs on port 4000
+      '/api': "http://72.62.227.2:4000"  // Backend runs on port 4000
     }
   },
   resolve: {
