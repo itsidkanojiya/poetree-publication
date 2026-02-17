63c63c9b532e9c7a40f199730b77e7cd7896d4d1
woopsa-sid <sid@cdtsupport.in>
2026-02-17 15:27:28 +0530

    chnage url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': "https://api.poetreebackend.app/api"  // Backend runs on port 4000
+      '/api': "https://api.poetreepublications.com/api"  // Backend runs on port 4000
     }
   },
   resolve: {
